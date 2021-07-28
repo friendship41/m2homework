713c8170f2b260e0777d38918ce07a0c0ae47b18
ht-chchoi <chchoi@hdtel.co.kr>
2021-07-28 11:02:13 +0900

    initial commit

---
+++ src/main/java/com/friendship41/m2homework/M2homeworkApplication.java
@@ -0,0 +1,13 @@
+package com.friendship41.m2homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M2homeworkApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(M2homeworkApplication.class, args);
+  }
+
+}
